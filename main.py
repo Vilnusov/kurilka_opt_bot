1bfe46697638c70a3fa161512cd1feb21ade89fb
Vilnusov <alexorekhov2004@gmail.com>
2023-05-23 17:17:20 +0300

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 import httplib2
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
@@ -19,25 +19,39 @@ def get_service_sacc():
     return build('sheets', 'v4', http=creds_service)
 
 
+def get_background_color(cell):
+    resp = sheet.get(spreadsheetId=sheet_id, ranges=f"жидкости!{cell}", includeGridData=True).execute().get('sheets')
+    color = resp[0]['data'][0]['rowData'][0]['values'][0]['effectiveFormat']['backgroundColor']
+    for col in list(color):
+        if col == 'red' and color[col] == 1 and len(list(color)) == 1:
+            stat = False
+        else: stat = True
+    print(stat)
+
+
 service = get_service_sacc()
 sheet = service.spreadsheets()
 
 sheet_id = "13XBmtqHjl85CtgTnbnHCqTgOdT4liOSuiPPsMk81U2U"
 
-resp = sheet.values().get(spreadsheetId=sheet_id, range="жидкости!A4:D147").execute().get('values', [])
+resp = sheet.values().get(spreadsheetId=sheet_id, range="жидкости!A4:D131").execute().get('values', [])
+#get_background_color('C125')
+#jsonString = json.dumps(resp[0])
+#pprint(jsonString[0])
+liquids_name = []
+liquids_taste = []
 pprint(resp)
-liquids = []
 
 for value in resp:
     for val in value:
-        if val == 'От 100р' or val == '0' or len(value) == 1:
+        if val == 'От 100р' or val == '0' or len(value) <= 1:
             resp.remove(value)
 
-pprint(resp)
+#pprint(resp)
 
 for value in resp:
     for val in value:
         if val == 'Расценки':
-            liquids.append(value[0])
+            liquids_name.append(value[0])
 
-pprint(liquids)
+#pprint(liquids_name)
